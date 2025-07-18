9dc4233b5075bdf387c018689311e204bd8b0dc9
Drakonkinst <drakonkinstmc@gmail.com>
2025-07-18 08:27:24 -0700

    Add a timer for surface alignment to kick in

--- player/player.gd
+++ player/player.gd
@@ -4,23 +4,32 @@ class_name Player
 
 @onready var input_state: InputState = %InputState
 @onready var nearby_surface_detection: NearbySurfaceDetection = %NearbySurfaceDetection
+@onready var surface_alignment_timer: Timer = %SurfaceAlignmentTimer
 var movement_speed := 100
 var look_sensitivity := 50.0
 var roll_speed := 80
 var surface_align_speed_when_still := 1.5
-var surface_align_speed_when_moving := 0.25
+var surface_align_speed_when_moving := 0.1
+var should_surface_align_quickly := true
 var touching_surface := false
 
 func _ready() -> void:
     input_state.mouse_looked.connect(_on_input_state_mouse_looked)
+    surface_alignment_timer.timeout.connect(_on_surface_alignment_timer_timeout)
     angular_damp = 1.0
     angular_damp_mode = DAMP_MODE_COMBINE
 
+func _on_surface_alignment_timer_timeout():
+    should_surface_align_quickly = true
+    
 func _on_input_state_mouse_looked(look_dir: Vector2):
     var right := -global_transform.basis.x
     var down := -global_transform.basis.y
     var delta_x := look_dir.y * look_sensitivity
     var delta_y := look_dir.x * look_sensitivity
+    print(look_dir.length_squared())
+    if look_dir.length_squared() > 0.0001:
+        _pause_surface_alignment(1.5)
     rotate(down, deg_to_rad(delta_y))
     rotate(right, deg_to_rad(delta_x))
 
@@ -31,6 +40,8 @@ func _physics_process(delta: float) -> void:
     var up := global_transform.basis.y
     var nearby_surface_vector := nearby_surface_detection.nearby_surface_vector
     var is_still := input_vector.is_zero_approx() && input_state.roll_input == 0
+    if !is_still:
+        _pause_surface_alignment(1.0)
     
     var move_dir := ((left * input_vector.x) + (up * input_vector.y) + (forward * input_vector.z)).normalized()
     apply_central_force(movement_speed * move_dir * delta)
@@ -45,5 +56,10 @@ func _physics_process(delta: float) -> void:
         axis = axis.normalized()
         
         var target_basis := global_transform.basis.rotated(axis, alpha)
-        var surface_align_speed := surface_align_speed_when_still if is_still else surface_align_speed_when_moving
+        var surface_align_speed := surface_align_speed_when_still if should_surface_align_quickly else surface_align_speed_when_moving
+        print(surface_align_speed)
         global_transform.basis = global_transform.basis.slerp(target_basis, surface_align_speed * delta)
+
+func _pause_surface_alignment(time: float):
+    should_surface_align_quickly = false
+    surface_alignment_timer.start(max(surface_alignment_timer.time_left, time))
